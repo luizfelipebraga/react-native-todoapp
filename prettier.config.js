a625bff1964abcc0b7a6d1c7e12ed924f2315cfc
luizfelipebraga <ruivolfb@gmail.com>
2021-12-04 14:28:20 -0300

    update(package)

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+export const options = {
+    arrowParents: 'avoid',
+    singleQuote: true,
+    backetSpacing: true,
+    endOfLine: 'lf',
+    semi: false,
+    tabWidth: '2',
+    trallingComma: 'none'
+}
